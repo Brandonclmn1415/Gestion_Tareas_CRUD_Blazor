74b7b9e669dee5f8b1cefa7436a7d8e93d827f5a
Brandonclmn1415 <brrandonlol1415@gmail.com>
2025-12-18 11:05:47 -0500

    Actualizacion SQL

---
+++ SQL/Scripts/SQLQuery_Datos_Ejemplo.sql
@@ -0,0 +1,8 @@
+
+-- Datos de ejemplo
+
+INSERT INTO Usuarios (Nombre, Apellido, Email, Usuario, Contraseña) VALUES
+('Juan', 'Pérez', 'juan.perez@email.com','JuanPerez','juan123'),
+('María', 'Gómez', 'maria.gomez@email.com','MariaGomez','maria123'),
+('Carlos', 'Rodríguez', 'carlos.rodriguez@email.com','CarlosRodriguez','carlos123');
+
